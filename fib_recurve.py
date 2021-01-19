58bb1a02aaa1cd1499f659f9cb5168ab458944cb
Noah Adkin <50452002+NAdkin@users.noreply.github.com>
2021-01-19 12:10:58 -0500

    stuff

--- fib_recurve.py
+++ fib_recurve.py
@@ -17,9 +17,28 @@ def fib2(x):
         memo[x] = r
         return r
 
-
 memo = {}
-print(fib(7))
-print(fib2(7))
+
+
+def grid_trav(m,n):
+    key = (str(m) + ',' + str(n))
+
+    if(key in memo):
+        return memo[key]
+    if(m == 1 and n == 1):
+        return 1
+    if(m == 0 or n == 0):
+        return 0
+
+    memo[key] = grid_trav(m - 1, n) + grid_trav(m, n - 1)
+    return memo[key]
+
+#print(grid_trav(1, 1))
+print(grid_trav(2, 3))
+print(memo)
+#print(grid_trav(3, 2))
+#print(grid_trav(18, 18))
+
+#print(fib2(40))
 
 
